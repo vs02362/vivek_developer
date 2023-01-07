04a71486fa77a7b830513a95369d7afb095647a3
vivek sharma <vs02362@gmail.com>
2023-01-07 22:26:07 +0530

    JavaScript

---
+++ JavaScript/classes.js
@@ -0,0 +1,12 @@
+class person{
+    constructor(fname,lname){
+        this.fname = fname;
+        this.lname = lname;
+    }
+
+    showDetails(){
+        console.log(this.fname + " " +this.lname);
+        
+    }
+
+}
